91f64b7339a86f363592985abdf612ddef176f84
George <you@example.com>
2022-12-05 18:28:45 +0300

    Don't open COM port monitor in Arduino IDE

--- main.py
+++ main.py
@@ -8,47 +8,51 @@ class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('interface.ui', self)
-        self.connections()
+        self.connections() #just not to cahnge __init__
         self.show()
 
     def connections(self):
         self.V1_b.stateChanged.connect(self.updateNumber)
-        self.Arduino()
+        self.COM_setup()
 
 
 
 
     def updateNumber(self, state):
+        print(1)
         if state == Qt.Checked:
             from randomer import RNG
-            print(self.serial.readLine())
-            self.V1.display(self.serial.readLine())
+            #print(self.serial.readLine())
+            print(2)
+            print(self.data)
+            self.V1.display(float(self.data))
 
         else:
             self.V1.display(0)
 
-    def Arduino(self):
+    def COM_setup(self):
         self.serial = QSerialPort()
-        self.serial.setBaudRate(9600)
+        self.serial.setBaudRate(QSerialPort.Baud9600)
         portList = []
         ports = QSerialPortInfo().availablePorts()
         for port in ports:
             portList.append(port.portName())
         #print(portList)
         port = portList[1]
-        print(port)
+        #print(port)
         #self.comL.addItems(portList)
         self.serial.setPortName(port)
+        self.serial.setDataTerminalReady(True)
         self.serial.open(QIODevice.ReadWrite)
-        #self.serial.write(b'551565')
+        #self.serial.write(b'1')
         self.serial.readyRead.connect(self.onRead)
 
 
 
 
     def onRead(self):
-        print(self.serial.readLine())
-
+        data = str(self.serial.readAll(), 'utf-8')
+        #print(bool(str(data, 'utf-8')))
 
 if __name__ == "__main__":
     import sys
