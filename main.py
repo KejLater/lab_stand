bf15dd92a5cefb1771d709712aa0c9352f1d2fcd
George <you@example.com>
2022-12-05 19:49:28 +0300

    Import slightly optimized Trying to embed multithreading

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtCore import Qt, QThread, pyqtSignal
-
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QIODevice
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
-from PyQt5.QtCore import QIODevice
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('interface.ui', self)
-        self.connections() #just not to cahnge __init__
+        self.connections()    #just not to cahnge __init__ anymore
         self.show()
 
     def connections(self):
-        self.V1_checkbox.stateChanged.connect(self.updateNumber)
-        self.V2_checkbox.stateChanged.connect(self.updateNumber1)
+        self.V1_checkbox.stateChanged.connect(self.update_V1)
+        self.V2_checkbox.stateChanged.connect(self.update_V2)   #test function
         self.COM_setup()
 
 
 
 
-    def updateNumber(self, state):
+    def update_V1(self, state):
         if state == Qt.Checked:
             from randomer import RNG
             #print(self.serial.readLine())
@@ -29,7 +27,7 @@ class Ui(QtWidgets.QMainWindow):
         else:
             self.V1.display(0)
 
-    def updateNumber1(self, state):
+    def update_V2(self, state):    #test function
         if state == Qt.Checked:
             from randomer import RNG
             #print(self.serial.readLine())
@@ -40,14 +38,11 @@ class Ui(QtWidgets.QMainWindow):
             self.V2.display(0)
 
     def COM_setup(self):
-        self.serial = QSerialPort()
+        self.serial = QSerialPort()   #Creating object of class QSerialPort
         self.serial.setBaudRate(QSerialPort.Baud9600)
-        portList = []
-        ports = QSerialPortInfo().availablePorts()
-        for port in ports:
-            portList.append(port.portName())
-        #print(portList)
-        port = portList[1]
+        ports = [port.portName() for port in QSerialPortInfo().availablePorts()]
+        #print(ports)
+        port = ports[1]
         #print(port)
         #self.comL.addItems(portList)
         self.serial.setPortName(port)
@@ -60,8 +55,23 @@ class Ui(QtWidgets.QMainWindow):
 
 
     def onRead(self):
-        self.data = str(self.serial.readAll(), 'utf-8').strip()
+        self.data = str(self.serial.readAll(), 'utf-8').strip()    #Turning bytes to str withuot '\n'
         #print(data)
+
+
+class MyThread(QThread):
+    mySignal = pyqtSignal(int)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.val = 0
+
+    def run(self):
+        while True:
+            self.val += 1  # Получаем определённые данные
+            self.mySignal.emit(self.val)  # Передаем данные для отображения
+            QThread.msleep(1000)
+
 
 if __name__ == "__main__":
     import sys
