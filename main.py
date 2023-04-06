1299920607654513cf0d29791e100a28530ffb7f
kejlater <antoshkingd@gmail.com>
2023-04-06 16:57:28 +0300

    Code optimized

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys, os
+
 from PyQt5 import QtWidgets, uic, QtGui
 from PyQt5.QtCore import Qt, QThread, pyqtSignal, QIODevice
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+
 import pandas as pd
 
 
@@ -10,7 +12,10 @@ class Data:    # class for interaction with DataFrame (df) and tableWidget (tabl
                 # instead of adding new data to table I clear it and fill again by function updateTable()
 
     def __init__(self):
+
         self.sorting_order = True    # reverses sorting order
+        self.meter_names = ['V1', 'V2', 'V3', 'V4', 'A1', 'A2', 'A3', 'A4']    # names for columns
+        self.df = pd.DataFrame(columns=self.meter_names)  # creates df to save results
 
     def sort_by(self, name):    # sorts data in df
 
@@ -20,19 +25,12 @@ class Data:    # class for interaction with DataFrame (df) and tableWidget (tabl
             self.sorting_order = not(self.sorting_order)    # reverses sorting order during next call
             self.updateTable()    #clears table and fills it with updated df
 
+
     def export_csv(self):    # exports df to csv
 
         dir = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File', filter='*.csv')[0]    # calls dialogue window
         if dir:    # checks if user did not cancel export
             self.df.to_csv(dir, index=False)
-
-
-    def clear_table(self):    # clears table but not df, adds column names to table
-
-        self.tableWidget.clear()    #clears table completly
-        headers = self.df.columns.values.tolist()    # exports names of columns for table from df
-        self.tableWidget.setHorizontalHeaderLabels(headers)    # adds columnss to table
-        self.tableWidget.setRowCount(0)    # makes RowCount equal to zero
 
 
     def resetTable(self):    # clears both table and df
@@ -41,7 +39,7 @@ class Data:    # class for interaction with DataFrame (df) and tableWidget (tabl
 
         if button == QtWidgets.QMessageBox.Yes:    # if user confirmed
 
-            self.df = pd.DataFrame(columns=[meter.objectName() for meter in self.meters])    # creates new df
+            self.df = pd.DataFrame(columns=self.meter_names)    # creates new df
             self.updateTable()    # clears table and fills it with updated df
 
     def removeLast(self):    # dropes the last row from both dataframe and table
@@ -59,7 +57,8 @@ class Data:    # class for interaction with DataFrame (df) and tableWidget (tabl
 
     def updateTable(self):    # clears table and fills it with updated df
 
-        self.clear_table()    # clears table but not df, adds column names to table
+        self.tableWidget.clearContents()  # clears table completly but not df
+        self.tableWidget.setRowCount(0)  # makes rowCount (hor dimension) equal to zero
 
         for i, row in self.df.iterrows():    # iterates the whole df
 
@@ -139,13 +138,9 @@ class MainWindow(QtWidgets.QMainWindow, Data):    # class with program and COM-p
         self.connected = False    # variable to track connection status
         self.data = '0@0@0@0@0@0@0@0'    # primary data for meters
 
-        self.df = pd.DataFrame(columns=[meter.objectName() for meter in self.meters])    # creates df to save results
-        self.choose_X.addItems(
-            [meter.objectName() for meter in self.meters])    # adds V1, V2... to list where user chooses X
-        self.choose_Y.addItems(
-            [meter.objectName() for meter in self.meters])    # adds V1, V2... to list where user chooses Y
-        self.choose_sort.addItems(
-            [meter.objectName() for meter in self.meters])     # adds V1, V2... to list where user chooses column to sort
+        self.choose_X.addItems(self.meter_names)    # adds V1, V2... to list where user chooses X
+        self.choose_Y.addItems(self.meter_names)    # adds V1, V2... to list where user chooses Y
+        self.choose_sort.addItems(self.meter_names)     # adds V1, V2... to list where user chooses column to sort
         self.exportCSV.clicked.connect(self.export_csv)    # ties button to function
         self.reset.clicked.connect(self.resetTable)    # ties button to function
         self.remove_last.clicked.connect(self.removeLast)    # ties button to function
@@ -188,7 +183,13 @@ class MainWindow(QtWidgets.QMainWindow, Data):    # class with program and COM-p
         if self.connected:    # checks if port is connected
 
             self.serial.readyRead.connect(self.onRead)    # makes onRead constantly work if True
-            self.show_MC_connected()    # changes color and title to "Подключено" if True
+
+            self.connect_label.setText('подключено')    # changes color and title to "Подключено" if True
+            self.connect_label.setStyleSheet("background-color: lightgreen")
+
+        else:
+            self.connect_label.setText('ошибка')    # changes color and title to "Ошибка" if False
+            self.connect_label.setStyleSheet("background-color: red")
 
 
     def onRead(self):    # constantly reads data from port and makes meters update
@@ -219,7 +220,14 @@ class MainWindow(QtWidgets.QMainWindow, Data):    # class with program and COM-p
         if self.connected:    # checks if port is connected
             self.serial.close()    # closes conection
             self.connected = False    # turns status variable to False
-            self.show_MC_disconnected()    # makes changes to color and title
+
+            if not self.connected:
+                self.connect_label.setText('не подключено')    # makes changes to color and title if True
+                self.connect_label.setStyleSheet("background-color: red")
+
+            else:
+                self.connect_label.setText('ошибка')    # changes color and title to "Ошибка" if False
+                self.connect_label.setStyleSheet("background-color: red")
 
 
     def multiplyString(self, string):    # this function is not used
@@ -250,53 +258,31 @@ class MainWindow(QtWidgets.QMainWindow, Data):    # class with program and COM-p
 
     def data_converter(self):    # convertes string with seven @ to list
 
-        if '@' in self.data:    # checks if @ is in string
+        if self.data.count("@") == 7:    # checks if seven @ are in string
             array = self.data.split('@')    # splits string
             return [round(float(n), 2) for n in array]    # turns elements of list from str to rounded float
-            return array
 
         else:
             return [0 for _ in range(8)]    # if @ are not in list returns list of 0
 
 
-    def show_MC_disconnected(self):
+    def updateAll(self):    # updates meters switched by user
 
-        if not self.connected:
-            self.connect_label.setText('не подключено')
-            self.connect_label.setStyleSheet("background-color: red")
+        values = self.data_converter()    # gets kist with eight floats
 
+        for i in range(0, len(values)):    # iterates values by index
 
-    def show_MC_connected(self):    #Init of functions which do not work without connection
+            if self.checkboxes[i].isChecked():    # checks if user switched on meter
 
-        if self.connected:
-            self.connect_label.setText('подключено')
-            self.connect_label.setStyleSheet("background-color: lightgreen")
-
-            self.setVoltage.clicked.connect(lambda: self.serialSend(self.inputVoltage.text()))
-            self.auto_launch.clicked.connect(lambda: self.auto_vac(self.auto_begin.text(), self.auto_end.text(), self.auto_step.text()))
-
-        else:
-            self.connect_label.setText('ошибка')
-            self.connect_label.setStyleSheet("background-color: red")
-
-
-    def updateAll(self):
-
-        values = self.data_converter()
-
-        for i in range(0, len(values)): #excluding V1
-
-            if self.checkboxes[i].isChecked():
-
-                self.meters[i].display(values[i])
+                self.meters[i].display(values[i])    # displays float
 
             else:
 
-                self.meters[i].display(0)
+                self.meters[i].display(0)    # turns meter to zero if not choosen
 
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":    # launches program
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     app.exec_()
