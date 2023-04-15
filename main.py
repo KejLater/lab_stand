ac166be727f85183ec381cd04804f0e8fb0d1f5f
kejlater <antoshkingd@gmail.com>
2023-04-15 19:46:14 +0300

    class Data was put to randomer.py and imported in main.py

--- main.py
+++ main.py
@@ -2,163 +2,61 @@ import sys, os
 from PyQt5 import QtWidgets, uic, QtGui
 from PyQt5.QtCore import Qt, QThread, pyqtSignal, QIODevice
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
-import pandas as pd
 
+from randomer import Data
 
-class Data:    # class for interaction with DataFrame (DF) and tableWidget (table) where data is stored
-               # visible tableWidget (table) is just derivative of DF
-               # instead of adding new data to table I clear it and fill again by function update_tableWidget()
-
-    def __init__(self):
-        self.sortingOrder = True  # reverses sorting order
-        self.meterNames = ['V1', 'V2', 'V3', 'V4', 'A1', 'A2', 'A3', 'A4']  # names for columns
-        self.DF = pd.DataFrame(columns=self.meterNames)  # creates DF to save results
-
-
-    def sort_df_by_column(self, name):  # sorts data in DF
-
-        if self.DF.size:  # checks if DF is not empty
-
-            self.DF = self.DF.sort_values(by=name, ignore_index=True, ascending=self.sortingOrder)
-            self.sortingOrder = not (self.sortingOrder)  # reverses sorting order during next call
-            self.update_tableWidget()  # clears table and fills it with updated DF
-
-
-    def export_df_to_csv(self):  # exports DF to csv
-
-        dir = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File', filter='*.csv')[0]  # calls dialogue window
-        if dir:  # checks if user did not cancel export
-            self.DF.to_csv(dir, index=False, decimal=',', sep=';')
-
-
-    def reset_df_and_table(self):  # clears both table and DF
-
-        button = QtWidgets.QMessageBox.question(self, "Подтверждение", "Really?")  # confirmation from user
-
-        if button == QtWidgets.QMessageBox.Yes:  # if user confirmed
-
-            self.DF = pd.DataFrame(columns=self.meterNames)  # creates new DF
-            self.update_tableWidget()  # clears table and fills it with updated DF
-
-
-    def remove_last_from_df(self):  # dropes the last row from both dataframe and table
-
-        if self.DF.size:  # checks if table is not empty
-
-            self.DF = self.DF.drop(labels=[len(self.DF) - 1], axis=0)
-            self.update_tableWidget()  # clears table and fills it with updated DF
-
-
-    def add_data_to_df(self):  # adds line of values to the end of DF
-
-        self.DF.loc[len(self.DF)] = [meter.value() for meter in self.meters]  # adds data from meters to DF
-        self.update_tableWidget()  # clears table and fills it with updated DF
-
-
-    def update_tableWidget(self):  # clears table and fills it with updated DF
-
-        self.tableWidget.clearContents()  # clears table completly but not DF
-        self.tableWidget.setRowCount(0)  # makes rowCount (horizontal dimension) equal to zero
-
-        for i, row in self.DF.iterrows():  # iterates the whole DF
-
-            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)  # adds new row to table
-
-            for j in range(self.tableWidget.columnCount()):  # iterates table
-
-                self.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(row[j])))  # sets item [i, j]
-
-
-    def build_graph(self):
-
-        Vs = [self.V1_y, self.V2_y, self.V3_y, self.V4_y]
-        Is = [self.A1_y, self.A2_y, self.A3_y, self.A4_y]
-        CBxs = Vs + Is
-
-        colors = ['#FF7F50', '#A52A2A', '#458B00', '#20B2AA', '#1E90FF', '#800080', '#FF3E96', '#7F7F7F']
-        names = ' '
-        n = 0
-        if self.DF.size and any([box.isChecked() for box in CBxs]):
-
-            from matplotlib import pyplot as plt
-            plt.axhline(y=0, color='black', linewidth=0.5)
-            x = self.DF[self.choose_X.currentText()]
-            if 'V' in self.choose_X.currentText():
-                plt.xlabel(f'{self.choose_X.currentText()}, В')
-            elif "A" in self.choose_X.currentText():
-                plt.xlabel(f'{self.choose_X.currentText()}, мА')
-            if any([box.isChecked() for box in Vs]):
-                for box in Vs:
-                    if box.isChecked():
-                        y = self.DF[box.objectName()[0:-2]]
-                        plt.plot(x, y, color=colors[n], label=box.objectName()[0:-2], marker='o', markersize=4)
-                        names = names + f'{box.objectName()[0:-2]}, '
-                        n = n + 1
-                plt.ylabel(f"{names} В")
-            else:
-                for box in Is:
-                    if box.isChecked():
-                        y = self.DF[box.objectName()[0:-2]]
-                        plt.plot(x, y, color=colors[n], label=box.objectName()[0:-2], marker='o', markersize=4)
-                        names = names + f'{box.objectName()[0:-2]}, '
-                        n = n + 1
-                plt.ylabel(f"{names} мА")
-            plt.legend()
-            plt.grid()
-            plt.show()
-
-
-
-class SerialPort:
+class SerialPort:  # class for interaction with port
 
     def __init__(self):
 
-        self.serial = QSerialPort()
-        self.update_ports()    # when proram opens, list of ports gets added to menu
+        self.serial = QSerialPort()  # creates object of class to deal with connections
+        self.update_ports()    # updates list of available ports
         self.data = '0@0@0@0@0@0@0@0'    # data for initialisation
 
 
-    def update_ports(self):
-        self.portList.clear()
-        self.portList.addItems([port.portName() for port in QSerialPortInfo().availablePorts()])
+    def update_ports(self):  # updates list of available ports
+        self.portList.clear()  # clears widget
+        self.portList.addItems([port.portName() for port in QSerialPortInfo().availablePorts()])  # adds ports
+                                                                                                  # to widget
 
 
-    def open_selected_port(self, selected_port):
-        if self.serial.isOpen():
+    def open_selected_port(self, selected_port):  # opens port chosen in widget
+
+        if self.serial.isOpen():  # closes port if it was previously opened
             self.serial.close()
 
-        if selected_port in [port.portName() for port in QSerialPortInfo().availablePorts()]:
-            self.serial = QSerialPort()
-            self.serial.setBaudRate(QSerialPort.Baud9600)
-            self.serial.setPortName(selected_port)
-            self.serial.setDataTerminalReady(True)
+        if selected_port in [port.portName() for port in QSerialPortInfo().availablePorts()]:  # checks if port lost
+
+            self.serial.setBaudRate(QSerialPort.Baud9600)  # sets BaudRate
+            self.serial.setPortName(selected_port)  # selects port
+            self.serial.setDataTerminalReady(True)  # prepares port
             self.serial.open(QIODevice.ReadWrite)  # opens port for interaction
 
-        else:
+        else:  # updates port list if chosen port lost
             self.update_ports()
 
-        if self.serial.isOpen():
+        if self.serial.isOpen():  # makes fuction read data from port and changes UI if successful
             self.serial.readyRead.connect(self.read_port)
             self.show_port_opened()
 
-        else:
+        else:  # makes UI show error if problems occured
             self.show_port_error()
 
 
-    def read_port(self):
-        if self.serial.canReadLine():
+    def read_port(self):  # reads data from port
+        if self.serial.canReadLine():  # checks if data is readable
             self.data = str(self.serial.readLine(), 'utf-8').strip()    # turns bytes to str withuot '\n'
 
 
-    def close_port(self):
+    def close_port(self):  # closes port
 
-        if self.serial.isOpen():
-            self.serial.close()
+        if self.serial.isOpen():  # checks if port is opened
+            self.serial.close()  # closes port
 
-        if not self.serial.isOpen():
+        if not self.serial.isOpen():  # makes changes to UI if port is closed
             self.show_port_closed()
 
-        else:
+        else:  # makes UI show error if port is still opened
             self.show_port_error()
 
 
--- randomer.py
+++ randomer.py
@@ -1,5 +1,110 @@
+import pandas as pd
+from PyQt5 import QtWidgets
+
+class Data:  # class for interaction with DataFrame (DF) and tableWidget (table) where data is stored
+             # visible tableWidget (table) is just derivative of DF
+             # instead of adding new data to table I clear it and fill again by function update_tableWidget()
+             # len(delf.DF) - number of rows
+
+    def __init__(self):
+        self.sortingOrder = True  # reverses sorting order
+        self.meterNames = ['V1', 'V2', 'V3', 'V4', 'A1', 'A2', 'A3', 'A4']  # label for columns
+        self.DF = pd.DataFrame(columns=self.meterNames)  # creates DF to store results
 
 
+    def sort_df_by_column(self, name):  # sorts data in DF
+
+        if len(self.DF):  # checks if DF is not empty
+
+            self.DF = self.DF.sort_values(by=name, ignore_index=True, ascending=self.sortingOrder)
+            self.sortingOrder = not (self.sortingOrder)  # reverses sorting order for next call
+            self.update_tableWidget()  # clears table and fills it with updated DF
 
 
+    def export_df_to_csv(self):  # exports DF to csv
+
+        dir = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File', filter='*.csv')[0]  # calls dialogue window
+        if dir:  # checks if user did not cancel export
+            self.DF.to_csv(dir, index=False, decimal=',', sep=';')
+
+
+    def reset_df_and_table(self):  # clears both table and DF
+
+        button = QtWidgets.QMessageBox.question(self, "Подтверждение", "Really?")  # confirmation from user
+
+        if button == QtWidgets.QMessageBox.Yes:  # if user confirmed
+
+            self.DF = pd.DataFrame(columns=self.meterNames)  # creates new DF rewriting variable
+            self.update_tableWidget()  # clears table and fills it with updated DF
+
+
+    def remove_last_from_df(self):  # dropes the last row from both dataframe and table
+
+        if len(self.DF):  # checks if table is not empty
+
+            self.DF = self.DF.drop(labels=[len(self.DF) - 1], axis=0)
+            self.update_tableWidget()  # clears table and fills it with updated DF
+
+
+    def add_data_to_df(self):  # adds line of values to the end of DF
+
+        self.DF.loc[len(self.DF)] = [meter.value() for meter in self.meters]  # adds data from meters to DF
+        self.update_tableWidget()  # clears table and fills it with updated DF
+
+
+    def update_tableWidget(self):  # clears table and fills it with updated DF
+
+        self.tableWidget.clearContents()  # clears table completly but not DF
+        self.tableWidget.setRowCount(0)  # makes rowCount (horizontal dimension) equal to zero
+
+        for i, row in self.DF.iterrows():  # iterates the whole DF
+
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)  # adds new row to table
+
+            for j in range(self.tableWidget.columnCount()):  # iterates table by column
+
+                self.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(row[j])))  # sets item [i, j]
+
+
+    def build_graph(self):
+
+        Vs = [self.V1_y, self.V2_y, self.V3_y, self.V4_y]  # list of checkboxes for Y axis for V1-V4
+        Is = [self.A1_y, self.A2_y, self.A3_y, self.A4_y]  # list of checkboxes for Y axis for A1-A4
+        CBxs = Vs + Is
+
+        colors = ['#FF7F50', '#A52A2A', '#458B00', '#20B2AA',
+                  '#1E90FF', '#800080', '#FF3E96', '#7F7F7F'] # eight possible colors for lines
+        label = ''  # string for X and Y labels
+
+        if len(self.DF) and any([box.isChecked() for box in CBxs]):
+
+            from matplotlib import pyplot as plt
+            plt.axhline(y=0, color='black', linewidth=0.5)
+
+            x = self.DF[self.choose_X.currentText()]
+
+            if 'V' in self.choose_X.currentText():  # chooses if volts or mA are in label
+                plt.xlabel(f'{self.choose_X.currentText()}, В')
+            elif "A" in self.choose_X.currentText():
+                plt.xlabel(f'{self.choose_X.currentText()}, мА')
+
+            if any([box.isChecked() for box in Vs]):
+                for box in Vs:
+                    if box.isChecked():
+                        y = self.DF[box.objectName()[0:-2]]
+                        plt.plot(x, y, colors.pop(), label=box.objectName()[0:-2], marker='o', markersize=4)
+                        label = label + f'{box.objectName()[0:-2]}, '
+
+                plt.ylabel(f"{label} В")
+            else:
+                for box in Is:
+                    if box.isChecked():
+                        y = self.DF[box.objectName()[0:-2]]
+                        plt.plot(x, y, colors.pop(), label=box.objectName()[0:-2], marker='o', markersize=4)
+                        label = label + f'{box.objectName()[0:-2]}, '
+
+                plt.ylabel(f"{label} мА")
+            plt.legend()
+            plt.grid()
+            plt.show()
 
